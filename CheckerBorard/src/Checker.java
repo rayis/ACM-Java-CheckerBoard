402b258e3480003595bdebbc421fab9490a95700
rayis <rayis@users.noreply.github.com>
2015-04-17 01:18:33 +0200

    Update Checker.java

--- CheckerBorard/src/Checker.java
+++ CheckerBorard/src/Checker.java
@@ -5,16 +5,16 @@ import acm.graphics.*;
 
 public class Checker extends GraphicsProgram {
 
-		public void run() {
+	public void run() {
 			
-				for(int i = 0; i < N_ROWS; i++) {
-					double sqSize = (double) getHeight() / N_ROWS;
-					double position = (double) getWidth() / 2  - (N_COLUMNS * sqSize) / 2;
-					double offset = (sqSize - sqSize / CHECKER_RATIO) / 2;
-					for(int j = 0; j < N_COLUMNS; j++) {
-						double x = j * sqSize + position;
-						double y = i * sqSize;
-						GRect rect = new GRect(x, y, sqSize, sqSize);
+		for(int i = 0; i < N_ROWS; i++) {
+			double sqSize = (double) getHeight() / N_ROWS;
+			double position = (double) getWidth() / 2  - (N_COLUMNS * sqSize) / 2;
+			double offset = (sqSize - sqSize / CHECKER_RATIO) / 2;
+				for(int j = 0; j < N_COLUMNS; j++) {
+					double x = j * sqSize + position;
+					double y = i * sqSize;
+					GRect rect = new GRect(x, y, sqSize, sqSize);
 						if((i + j) % 2 != 0 ) {
 							rect.setFilled(true);
 							rect.setFillColor(Color.GRAY);
